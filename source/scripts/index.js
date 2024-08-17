7a7be7d54112f324c6bfec809de0609de948122c
KatrinMul <115039817+KatrinMul@users.noreply.github.com>
2024-08-17 23:03:40 +0300

    Module6-task1 - добавление крупных сеток (#7)
    * Разметка сетки навигации
    
    * добавление крупных сеток главной страницы
    
    * добавление крупных сеток главной страницы - продолжение
    
    * добавление крупных сеток главной страницы - продолжение
    
    * добавление крупных сеток главной страницы завершено

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,9 @@
-/* в этот файл добавляет скрипты*/
+const nav = document.querySelector('.nav');
+const navButton = document.querySelector('.nav__button');
+
+nav.classList.add('nav--closed');
+
+navButton.addEventListener('click', () => {
+  nav.classList.toggle('nav--closed');
+  nav.classList.toggle('nav--opened');
+});
