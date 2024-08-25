9fec94f70e2a279f59aa2bfb1df3820b4cff394f
KatrinMul <115039817+KatrinMul@users.noreply.github.com>
2024-08-25 19:38:43 +0300

    Module7-task1 добавление слайдера и стилизация каталога (#8)
    * Добавление слайдера
    
    * Стилизация каталога - мобилка
    
    * Стилизация каталога - планшет и десктоп

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -7,3 +7,21 @@ navButton.addEventListener('click', () => {
   nav.classList.toggle('nav--closed');
   nav.classList.toggle('nav--opened');
 });
+
+const slider = document.querySelector('.slider');
+const sliderButton = document.querySelector('.slider__toggle');
+
+if(slider){
+  flag = false;
+  sliderButton.addEventListener('click', () =>{
+    if(!flag) {
+      flag = true;
+      slider.classList.add('slider--before');
+    }else{
+      slider.classList.toggle('slider--before');
+      slider.classList.toggle('slider--after');
+    }
+  });
+}
+
+
